2bed145ab57c57b3ce3728b65a3e33de60aa0438
Heine574 <johannes@niemela.se>
2022-05-08 14:09:58 -0500

    switched to a different react app template

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import axios from 'axios';
+
+import API_KEY from './config.js';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    const { } = this.state;
+
+    return (
+      <>
+
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ client/webpack.config.js
@@ -0,0 +1,36 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+  entry: [
+    './src/index.jsx'
+  ],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  devServer: {
+    'static': {
+      directory: './dist'
+    }
+  }
+};
+
+module.exports = config;
